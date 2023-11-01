96fe12ed8fcd0cd6ab419258b9dd0ce7c2a7357b
Rupam <rupampal48@gmail.com>
2023-11-01 22:39:26 +0530

    8th commit - 8th video - Function Basics

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,21 @@
-console.log('test');
+let greet : Function;
+
+greet = ()=>{
+    console.log('Hello World');
+}
+
+const add = (a:number,b:number,c ?: number|string):void=>{
+    console.log(a+b);
+    console.log(c);
+    
+}
+
+add(5,10,'ram');
+
+const minus = (a:number,b:number):number=>{
+   return a+b;
+}
+
+let result = minus(10,7);
+console.log(result);
+
